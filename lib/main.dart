07b5ff4c8bcf6cb5dc889c7bd3862e9010d57f1d
souta11211125 <mizu1121so@icloud.com>
2025-06-16 13:50:32 +0900

    Revert "Todo single2(あと通知などをいれるだけ)"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
-import 'home_screen_souta.dart';
+import 'home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
